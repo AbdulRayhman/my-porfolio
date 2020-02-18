c7457b1459b5990a9ad5dd4307bef54affaa3b0f
Abdul Rehman <arehman@sparkcognition.com>
2020-02-18 19:10:01 +0500

    fix DB settings

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 # MONGO_URI = os.environ.get('MONGO_URI')
 # print(MONGO_URI)
 # app.config.from_object('some string')
-app.config["MONGO_URI"] = 'mongodb+srv://abdulrehman:abdulRehman@portfolio-kpqci.mongodb.net/portfolio?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://abdulrehmanFrt:abdulrehmanFrt@userdb-tywac.mongodb.net/portfolio?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
 @app.route('/')
